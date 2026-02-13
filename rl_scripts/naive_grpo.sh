1fb4f02e9fa4bc58c71118b6bd91b4a1e93fb05a
Abdelrahman721 <abdelrahmangalal721@gmail.com>
2026-02-13 09:38:25 +0000

    oom issues

--- rl_scripts/naive_grpo.sh
+++ rl_scripts/naive_grpo.sh
@@ -31,10 +31,10 @@ python3 -m verl.trainer.main_ppo \
   data.truncation='error' \
   actor_rollout_ref.actor.optim.lr=1e-6 \
   actor_rollout_ref.model.use_remove_padding=True \
-  actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+  actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
   actor_rollout_ref.actor.ppo_mini_batch_size="${PROMPT_BATCH}" \
-  actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
-  actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+  actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
+  actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
   actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
   actor_rollout_ref.rollout.name=vllm \
   actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
