56f68d913ed5319be43b2fab0e55ec6ee027c47e
Abdelrahman721 <abdelrahmangalal721@gmail.com>
2026-02-13 10:30:22 +0000

    that was a mistake

--- rl_scripts/naive_grpo.sh
+++ rl_scripts/naive_grpo.sh
@@ -32,7 +32,7 @@ python3 -m verl.trainer.main_ppo \
   actor_rollout_ref.actor.optim.lr=1e-6 \
   actor_rollout_ref.model.use_remove_padding=True \
   actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
-  actor_rollout_ref.actor.ppo_mini_batch_size=512 \
+  actor_rollout_ref.actor.ppo_mini_batch_size="${PROMPT_BATCH}" \
   actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
   actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
   actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
@@ -60,6 +60,6 @@ python3 -m verl.trainer.main_ppo \
   trainer.n_gpus_per_node=2 \
   trainer.rollout_data_dir=$HOME/verl_dumps/rollouts \
   trainer.validation_data_dir=$HOME/verl_dumps/val \
-  trainer.total_epochs=2 $@
+  trainer.total_epochs=1 $@
 
 
