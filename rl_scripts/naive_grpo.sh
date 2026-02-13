404a3d35dc71ac502c41366faa1735e6cbe8450d
Abdelrahman721 <abdelrahmangalal721@gmail.com>
2026-02-13 10:24:19 +0000

    adjust mini batch size

--- rl_scripts/naive_grpo.sh
+++ rl_scripts/naive_grpo.sh
@@ -32,7 +32,7 @@ python3 -m verl.trainer.main_ppo \
   actor_rollout_ref.actor.optim.lr=1e-6 \
   actor_rollout_ref.model.use_remove_padding=True \
   actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
-  actor_rollout_ref.actor.ppo_mini_batch_size="${PROMPT_BATCH}" \
+  actor_rollout_ref.actor.ppo_mini_batch_size=512 \
   actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
   actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
   actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
