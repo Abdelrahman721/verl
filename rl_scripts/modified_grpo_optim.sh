4c5ee9bcbd1197165dc00c9acf36451182faabc8
mbinakba-avey <mbinakba-avey@gmail.com>
2026-02-25 07:36:40 +0000

    optimized grpo++

---
+++ rl_scripts/modified_grpo_optim.sh
@@ -0,0 +1,109 @@
+set -x
+
+MODEL_PATH="Qwen/Qwen3-4B-Base"
+TRAIN_FILES="/workspace/verl/data/dataset_a.parquet"
+VAL_FILES="/workspace/verl/data/dataset_ab_common.parquet"
+
+PROJECT_NAME="ICD-RL"
+EXP_NAME="gen_16_epoch_100"
+
+MAX_PROMPT_LEN=512
+MAX_RESPONSE_LEN=4096
+
+TEMP=1.0
+TOP_P=1.0
+
+N_SAMPLES_PER_PROMPT=16
+
+TRAIN_BATCH_SIZE=64
+GEN_BATCH_SIZE=192
+
+CLIP_LOW=0.2
+CLIP_HIGH=0.28
+
+ROLLOUT_IS_LEVEL="token"
+ROLLOUT_IS_THRESHOLD=2.0
+
+FILTER_METRIC="seq_reward"
+MAX_NUM_GEN_BATCHES=10
+
+export WANDB_API_KEY="7eadd40652b0651b0f12dc86ea4d5fde56db2e2a"
+
+python3 -m verl.trainer.main_ppo \
+  algorithm.adv_estimator=grpo \
+  \
+  algorithm.norm_adv_by_std_in_grpo=False \
+  \
+  data.train_files="${TRAIN_FILES}" \
+  data.val_files="${VAL_FILES}" \
+  data.max_prompt_length="${MAX_PROMPT_LEN}" \
+  data.max_response_length="${MAX_RESPONSE_LEN}" \
+  data.filter_overlong_prompts=True \
+  data.truncation='error' \
+  \
+  data.train_batch_size="${TRAIN_BATCH_SIZE}" \
+  +data.gen_batch_size="${GEN_BATCH_SIZE}" \
+  +algorithm.filter_groups.enable=True \
+  +algorithm.filter_groups.metric="${FILTER_METRIC}" \
+  +algorithm.filter_groups.max_num_gen_batches="${MAX_NUM_GEN_BATCHES}" \
+  \
+  actor_rollout_ref.model.path="${MODEL_PATH}" \
+  actor_rollout_ref.model.use_remove_padding=True \
+  actor_rollout_ref.model.enable_gradient_checkpointing=True \
+  \
+  actor_rollout_ref.rollout.n="${N_SAMPLES_PER_PROMPT}" \
+  actor_rollout_ref.rollout.name=vllm \
+  actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
+  \
+  actor_rollout_ref.rollout.calculate_log_probs=True \
+  +algorithm.rollout_correction.rollout_is="${ROLLOUT_IS_LEVEL}" \
+  algorithm.rollout_correction.rollout_is_threshold="${ROLLOUT_IS_THRESHOLD}" \
+  algorithm.rollout_correction.bypass_mode=false \
+  algorithm.rollout_correction.rollout_rs=null \
+  algorithm.rollout_correction.rollout_rs_threshold=null \
+  +algorithm.rollout_correction.rollout_rs_threshold_lower=null \
+  +algorithm.rollout_correction.rollout_token_veto_threshold=null \
+  \
+  actor_rollout_ref.actor.loss_agg_mode="token-mean" \
+  \
+  actor_rollout_ref.actor.use_kl_loss=False \
+  algorithm.use_kl_in_reward=False \
+  \
+  actor_rollout_ref.actor.clip_ratio_low="${CLIP_LOW}" \
+  actor_rollout_ref.actor.clip_ratio_high="${CLIP_HIGH}" \
+  \
+  actor_rollout_ref.actor.ppo_epochs=1 \
+  actor_rollout_ref.actor.ppo_mini_batch_size="${TRAIN_BATCH_SIZE}" \
+  actor_rollout_ref.actor.use_dynamic_bsz=True \
+  actor_rollout_ref.actor.ppo_max_token_len_per_gpu=10240 \
+  actor_rollout_ref.actor.optim.lr=1e-6 \
+  actor_rollout_ref.actor.entropy_coeff=0 \
+  \
+  actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=True \
+  actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=24000 \
+  actor_rollout_ref.rollout.temperature="${TEMP}" \
+  actor_rollout_ref.rollout.top_p="${TOP_P}" \
+  actor_rollout_ref.ref.log_prob_use_dynamic_bsz=True \
+  actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=24000 \
+  actor_rollout_ref.ref.fsdp_config.param_offload=True \
+  actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+  actor_rollout_ref.rollout.max_num_batched_tokens=32768 \
+  actor_rollout_ref.rollout.max_num_seqs=512 \
+  actor_rollout_ref.rollout.enforce_eager=False \
+  actor_rollout_ref.rollout.free_cache_engine=True \
+  \
+  custom_reward_function.path="/workspace/verl/verl/utils/reward_score/icd.py" \
+  custom_reward_function.name="compute_score" \
+  \
+  trainer.critic_warmup=0 \
+  trainer.logger='["console","wandb"]' \
+  trainer.project_name="${PROJECT_NAME}" \
+  trainer.experiment_name="${EXP_NAME}" \
+  trainer.nnodes=1 \
+  trainer.n_gpus_per_node=8 \
+  trainer.save_freq=50 \
+  trainer.test_freq=5 \
+  trainer.total_epochs=100 \
+  trainer.rollout_data_dir=$HOME/verl_dumps/rollouts \
+  trainer.validation_data_dir=$HOME/verl_dumps/val \
+  "$@"
