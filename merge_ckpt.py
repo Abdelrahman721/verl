1c95713346040adc35fd8a598cee616c24f29a9b
elsayedhazemm <helsayed@avey.ai>
2026-03-03 01:25:25 +0300

    update merge path

--- merge_ckpt.py
+++ merge_ckpt.py
@@ -4,9 +4,9 @@ import os
 import torch
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 
-ckpt_dir = "/workspace/models/checkpoint-1.65k/actor"
+ckpt_dir = "/workspace/models/checkpoint-1.85k/actor"
 hf_config_dir = os.path.join(ckpt_dir, "huggingface")
-output_dir = "/workspace/models/checkpoint-1.65k/merged_hf"
+output_dir = "/workspace/models/checkpoint-1.85k/merged_hf"
 
 device = "cuda:0"
 
